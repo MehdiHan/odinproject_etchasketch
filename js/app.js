2d98e2358e2d2611da082e2348d4ea65bbd4df25
MehdiHan <meh.bouchard@outlook.fr>
2020-09-20 13:17:01 +0200

    Adding an input to customise grid template

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
-const etchSketch = () => {
-  const container = document.getElementById("app");
-  const resetButton = document.getElementById("reset");
-  const colorPicker = document.getElementById("colorPicker");
+const container = document.getElementById("app");
+const squareNumbers = document.getElementById("squareNumbers");
+const squareSetup = document.getElementById("squareSetup");
+const squareColorReset = document.getElementById("squareColorReset");
+const colorPicker = document.getElementById("squareColorPicker");
 
-  for (let i = 0; i < 100; i++) {
+const createArea = (area) => {
+  for (let i = 0; i < area ** 2; i++) {
     const div = document.createElement("div");
+    const width = 500 / area;
+    const height = 500 / area;
+
+    div.style.width = String(width) + "px";
+    div.style.height = String(height) + "px";
     div.classList.add("square");
+
     container.appendChild(div);
 
     div.addEventListener("mouseenter", function (event) {
-      event.target.style.backgroundColor = colorPicker.value;
+      event.target.style.backgroundColor = squareColorPicker.value;
     });
   }
-
-  resetButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-      square.removeAttribute("style");
-    });
-  });
 };
 
-etchSketch();
+squareSetup.addEventListener("click", function (event) {
+  container.innerHTML = "";
+  let squareSettings = squareNumbers.value;
+  createArea(squareSettings);
+});
+
+squareColorReset.addEventListener("click", function (event) {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.style.removeProperty("background-color");
+  });
+});
